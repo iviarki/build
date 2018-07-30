cd537fc434c79af27474eaa403c2e6e4c4c7a258
Mark Tomzynski <tomzynsk@amazon.com>
2018-07-30 13:26:48 +0200

    Bob

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -6,7 +6,7 @@ PROJECT_HOME="$(cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 echo $PROJECT_HOME
 
 mkdir -p $PROJECT_HOME
-sudo cat <<EOF > {$PROJECT_HOME}/.env
+sudo cat <<EOF > $PROJECT_HOME/.env
 PIPENV_VENV_IN_PROJECT=True
 PROJECT_HOME=$PROJECT_HOME
 AWS_PROFILE=mach
