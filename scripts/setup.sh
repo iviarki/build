7d69b4402a2eb9b57e32be9d843e1a962501d696
Mark Tomzynski <tomzynsk@amazon.com>
2018-07-30 09:53:13 +0200

    Removed sudo

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -2,7 +2,7 @@
 PROJECT_HOME="$(cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 echo $PROJECT_HOME
 
-sudo cat <<EOF > $PROJECT_HOME/.env
+cat <<EOF > $PROJECT_HOME/.env
 PIPENV_VENV_IN_PROJECT=True
 PROJECT_HOME=$PROJECT_HOME
 AWS_PROFILE=mach
